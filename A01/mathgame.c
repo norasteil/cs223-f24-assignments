8f24f26fd478d62d0e667f7a0c47c22202b7f3b7
Nora Steil <nsteil@brynmawr.edu>
2024-09-05 15:38:05 -0400

    Nora Steil A01

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,36 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Nora Steil
  * Implements a math game
  */
 
 #include <stdio.h>
+#include<stdlib.h>
+
+void game(int a) {
+	int correct = 0;
+	for (int i = 0; i < a; i++) {
+		int num1 = rand() % 10;
+		int num2 = rand() % 10;
+		int userAns;
+		printf("%d + %d = ? ", num1, num2);
+		scanf("%d", &userAns);
+		if (userAns == (num1 + num2)) {
+			printf("Correct!\n");
+			correct++;
+		}
+		else {
+			printf("Incorrect.\n");
+		}
+	}
+	printf("You correctly answered %d/%d\n", correct, a);
+}
 
 int main() {
-  return 0;
+	printf("Welcome to Math Game!\n");
+	int a;
+	printf("How many rounds do you want to play? ");
+	scanf("%d", &a);
+	game(a);
+	return 0;
 }
